ae1af0813e8178bb4a672ee0bbeed0a8de16187b
Fausto Saporito <fausto.saporito@gmail.com>
2016-04-08 13:08:17 +0200

    cleaning up

--- main.py
+++ main.py
@@ -4,16 +4,15 @@ import scipy.sparse as sp
 import numpy as np
 import time
 
-NUM = bin(2333)[2:]
-NUM = NUM[::-1] # reverse the input binary digits
-Ncoeff = 1.0/math.sqrt(2**len(NUM))
+NUM = bin(2333888)[2:]
+NUM = NUM[::-1]  # reverse the input binary digits
 pCoeff = 1.0/math.sqrt(2)
 a = []
 t = 0.0
-_k0 = np.array([1,0],dtype=complex)
-_k0.shape=(2,1)
-_k1 = np.array([0,1],dtype=complex)
-_k1.shape=(2,1)
+_k0 = np.array([1, 0], dtype=complex)
+_k0.shape = (2, 1)
+_k1 = np.array([0, 1], dtype=complex)
+_k1.shape = (2, 1)
 k0 = sp.coo_matrix(_k0)
 k1 = sp.coo_matrix(_k1)
 
@@ -21,10 +20,10 @@ t0 = time.time()
 
 for i in range(1, len(NUM) + 1):
     for j in range(1, i + 1):
-        # print(i,"-",j,"==",NUM[j-1])
-        expon = 2**(i-j+1) #should be negative
+
+        expon = 2**(i-j+1)  # should be negative
         t += int(NUM[j - 1]) * (2 ** -(i-j+1))
-        # print("expon:",expon,"t: ",t)
+
     t = cmath.exp(2 * cmath.pi * complex(0, 1) * t)
     a.append(t)
     t = 0.0
@@ -32,10 +31,9 @@ for i in range(1, len(NUM) + 1):
 z1 = pCoeff*sp.coo_matrix(_k0+a[0]*_k1)
 
 for i in a[1:]:
-    #print("Element with other a: ",i)
-    z1 = sp.kron(z1,pCoeff*(k0 + i*k1))
+    z1 = sp.kron(z1, pCoeff*(k0 + i*k1))
 
-print("Elapsed: ",time.time()-t0)
-print("Number of input qubit: ",len(NUM))
-print("QUREG length: ",(z1).shape[0])
+print("Elapsed: ", time.time()-t0)
+print("Number of input qubit: ", len(NUM))
+print("QUREG length: ", z1.shape[0])
 
